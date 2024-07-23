88d60613406d5c9a0c7509bb5df0208078084e19
James Caicedo <jacmlove19@hotmail.com>
2024-07-23 00:04:31 -0500

    Trabajando en Home

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import './Home.css';
-
+import buscar from './Assets/img/buscar.jpg';
+import ReactPlayer from 'react-player';
 const Home = () => {
+  const vidUrl = "https://www.youtube.com/watch?v=-rZGi1xjZNU"
   return (
     <div className='body-home'>
-        <div className='d-flex flex-column'>
-          
+      <div className='body-card'>
+        <div className='card-one'>
+          <img src={buscar} alt="imagen de empleo" />
+          <p>En esta aplicacion podras encontrar miles de ofertas laborales que se ajusten a tu perfirl laboral y de acuerdo a tu experiencia y aspiracion salarial. En Job trabajamos para que tengas una mejor opcion dia a dia. Job is Job.</p>
         </div>
+        <div className='card-two'>
+          <ReactPlayer 
+            url={vidUrl}
+            playing={false}
+            volume={0.5}
+            controls
+          />
+          <p>En esta aplicacion podras encontrar miles d ofertas laborales que se ajusten a tu perfirl laboral y de acuerdo a tu experiencia y aspiracion salarial. En Job trabajamos para que tengas una mejor opcion dia a dia. Job is Job.</p>
+        </div>
+       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
